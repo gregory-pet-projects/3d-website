1354afba0b53b12d85beab3413fbfc115f0f5034
GregoryPetrovLn <grishap96123@gmail.com>
2023-05-01 15:23:17 +0300

    fixed config project

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   mode: "jit",
   theme: {
     extend: {
