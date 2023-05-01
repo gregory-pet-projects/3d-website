b8944cc1c97178741b5d90d00512a2345b200c49
GregoryPetrovLn <grishap96123@gmail.com>
2023-05-01 14:50:36 +0300

    init structure empty components

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import About from "./About";
+import Contact from "./Contact";
+import Experience from "./Experience";
+import Feedbacks from "./Feedbacks";
+import Hero from "./Hero.jsx";
+import CanvasLoader from "./Loader";
+import Navbar from "./Navbar";
+import Tech from "./Tech";
+import Works from "./Works";
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  CanvasLoader,
+};
